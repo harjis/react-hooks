b04d958328d222ee7641d51ca82e74e0873c40b3
Joonas Harjukallio <harjukallio@gmail.com>
2019-12-14 11:10:36 +0200

    Remove not needed

--- src/hooks/useSearch.ts
+++ src/hooks/useSearch.ts
@@ -3,16 +3,14 @@ import React from 'react';
 type ReturnType<T> = { filteredItems: T[]; onSearch: (value: string) => void };
 export default function useSearch<T extends {}>(items: T[], key: keyof T): ReturnType<T> {
   const [filteredItems, setFilteredItems] = React.useState(items);
-  const initialItems = React.useRef(items);
   React.useEffect(() => {
     setFilteredItems(items);
-    initialItems.current = items;
   }, [items]);
 
-  const resetSearch = (): void => setFilteredItems(initialItems.current);
+  const resetSearch = (): void => setFilteredItems(items);
 
   const doSearch = (value: string): T[] =>
-    initialItems.current.reduce<T[]>((acc, filteredItem) => {
+    items.reduce<T[]>((acc, filteredItem) => {
       const filteredItemValue = filteredItem[key];
       if (
         typeof filteredItemValue === 'string' &&
