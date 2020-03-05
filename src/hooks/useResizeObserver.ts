838bc6021cd8e59c74a026fae373163a9ba1309b
Joonas Harjukallio <harjukallio@gmail.com>
2020-03-05 21:28:29 +0200

    Add useResizeObserver.ts

---
+++ src/hooks/useResizeObserver.ts
@@ -0,0 +1,37 @@
+import React from "react";
+import ResizeObserver from "resize-observer-polyfill";
+
+type Dimensions = {
+  height: number;
+  width: number;
+};
+type ReturnType<ElementType> = [React.RefObject<ElementType>, Dimensions];
+const useResizeObserver = <
+  ElementType extends Element
+>(): ReturnType<ElementType> => {
+  const [dimensions, setDimensions] = React.useState({ height: 0, width: 0 });
+  const ref = React.useRef<ElementType>(null);
+
+  React.useEffect(() => {
+    const observer = new ResizeObserver(([element]) => {
+      // https://alligator.io/js/resize-observer/
+      // Unlike with an element’s getBoundingClientRect, contentRect’s values for width and height don’t
+      // include padding values. contentRect.top is the element’s top padding
+      // and contentRect.left is the element’s left padding.
+      const width = element.contentRect.left + element.contentRect.right;
+      const height = element.contentRect.top + element.contentRect.bottom;
+
+      setDimensions({ height, width });
+    });
+
+    if (ref.current) observer.observe(ref.current);
+
+    return () => {
+      if (ref.current) observer.unobserve(ref.current);
+    };
+  });
+
+  return [ref, dimensions];
+};
+
+export default useResizeObserver;
---
+++ src/stories/05-useResizeObserver.tsx
@@ -0,0 +1,36 @@
+import React from "react";
+
+import useResizeObserver from "../hooks/useResizeObserver";
+
+export default {
+  title: "ResizeObserver"
+};
+
+export const UseResizeObserver = () => {
+  const [size, setSize] = React.useState({ height: 100, width: 100 });
+  const [ref, dimensions] = useResizeObserver<HTMLDivElement>();
+  const [ref2, dimensions2] = useResizeObserver<HTMLDivElement>();
+  return (
+    <div>
+      <button
+        onClick={() => {
+          setSize(prevState => ({
+            height: prevState.height + 100,
+            width: prevState.width + 100
+          }));
+        }}
+      >
+        Increase div size
+      </button>
+      <div style={{ height: size.height, width: size.width }} ref={ref}>
+        height: {dimensions.height} width: {dimensions.width}
+      </div>
+
+      <div style={{ height: 200, width: 250 }} ref={ref2}>
+        height: {dimensions2.height} width: {dimensions2.width}
+      </div>
+    </div>
+  );
+};
+
+UseResizeObserver.story = { name: "Default" };
