fc28aae7a923c1c42dbf8bc39a73a069fc942475
Joonas Harjukallio <harjukallio@gmail.com>
2020-09-09 09:32:01 +0300

    Add tests

--- src/__tests__/useFetchObservable.test.tsx
+++ src/__tests__/useFetchObservable.test.tsx
@@ -5,10 +5,12 @@ import useFetchObservable, { LoadingState } from "../hooks/useFetchObservable";
 
 const initialData: string[] = [];
 describe(useFetchObservable, () => {
+  const data = ["Response is here"];
+
   describe("when hook mounts", () => {
     let dummyQuery: () => Observable<string[]>;
     beforeAll(() => {
-      dummyQuery = () => from(dummyRequest());
+      dummyQuery = () => from(dummyRequest(data));
     });
     it("is not loading", async () => {
       const { result, waitForNextUpdate } = renderHook(() =>
@@ -36,6 +38,102 @@ describe(useFetchObservable, () => {
       expect(result.current.error).toEqual("");
       await waitForNextUpdate();
     });
+
+    describe("when query resolves", () => {
+      it("updates data", async () => {
+        const { result, waitForNextUpdate } = renderHook(() =>
+          useFetchObservable(dummyQuery, initialData)
+        );
+
+        await waitForNextUpdate();
+        expect(result.current.data).toEqual(data);
+      });
+
+      it("is loaded", async () => {
+        const { result, waitForNextUpdate } = renderHook(() =>
+          useFetchObservable(dummyQuery, initialData)
+        );
+
+        await waitForNextUpdate();
+        expect(result.current.loadingState).toEqual(LoadingState.LOADED);
+      });
+    });
+  });
+
+  describe("when hook receives updated query", () => {
+    let dummyQuery: () => Observable<string[]>;
+    let newDummyQuery: () => Observable<string[]>;
+    const newData = ["New response"];
+    beforeAll(() => {
+      dummyQuery = () => {
+        const observable = new Observable<string[]>((subscriber) => {
+          const timeout = setTimeout(() => subscriber.next(data), 1);
+          return () => clearTimeout(timeout);
+        });
+        return observable;
+      };
+
+      newDummyQuery = () => {
+        const observable = new Observable<string[]>((subscriber) => {
+          const timeout = setTimeout(() => subscriber.next(newData), 1);
+          return () => clearTimeout(timeout);
+        });
+        return observable;
+      };
+    });
+
+    it("updates data", async () => {
+      const { result, rerender, waitForNextUpdate } = renderHook(
+        ({ query, initialData }) => useFetchObservable(query, initialData),
+        { initialProps: { query: dummyQuery, initialData: initialData } }
+      );
+
+      await waitForNextUpdate();
+      expect(result.current.data).toEqual(data);
+      rerender({ query: newDummyQuery, initialData: [] });
+
+      await waitForNextUpdate();
+      expect(result.current.data).toEqual(newData);
+    });
+  });
+
+  describe("when hook receives updated query before last query has resolved", () => {
+    let dummyQuery: () => Observable<string[]>;
+    let newDummyQuery: () => Observable<string[]>;
+    const newData = ["New response"];
+    const spy = jest.fn();
+    beforeAll(() => {
+      dummyQuery = () => {
+        const observable = new Observable<string[]>((subscriber) => {
+          const timeout = setTimeout(() => subscriber.next(data), 1);
+          return () => {
+            clearTimeout(timeout);
+            spy();
+          };
+        });
+        return observable;
+      };
+
+      newDummyQuery = () => {
+        const observable = new Observable<string[]>((subscriber) => {
+          const timeout = setTimeout(() => subscriber.next(newData), 1);
+          return () => clearTimeout(timeout);
+        });
+        return observable;
+      };
+    });
+    it("unsubscribes to the old query and has new data", async () => {
+      const { result, rerender, waitForNextUpdate } = renderHook(
+        ({ query, initialData }) => useFetchObservable(query, initialData),
+        { initialProps: { query: dummyQuery, initialData: initialData } }
+      );
+
+      rerender({ query: newDummyQuery, initialData: [] });
+      expect(spy).toHaveBeenCalledTimes(1);
+
+      await waitForNextUpdate();
+      expect(result.current.data).toEqual(newData);
+    });
   });
 
   describe("when hook unmounts", () => {
@@ -45,8 +143,8 @@ describe(useFetchObservable, () => {
       dummyQuery = () => {
         const observable = new Observable<string[]>((subscriber) => {
           const timeout = setTimeout(() => {
-            subscriber.next(["str"]);
-          }, 100);
+            subscriber.next(data);
+          }, 1);
           return () => {
             spy();
             clearTimeout(timeout);
@@ -66,6 +164,6 @@ describe(useFetchObservable, () => {
   });
 });
 
-function dummyRequest() {
-  return Promise.resolve(["Response is here"]);
+function dummyRequest(data: string[]) {
+  return Promise.resolve(data);
 }
