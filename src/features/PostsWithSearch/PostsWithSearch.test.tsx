ac54a6e7056177c087108abbd9208228afb966f6
Joonas Harjukallio <harjukallio@gmail.com>
2021-02-05 15:19:55 +0200

    Better

--- src/features/PostsWithSearch/PostsWithSearch.test.tsx
+++ src/features/PostsWithSearch/PostsWithSearch.test.tsx
@@ -1,5 +1,10 @@
 import React from "react";
-import { render, waitFor } from "@testing-library/react";
+import {
+  render,
+  waitFor,
+  waitForElementToBeRemoved,
+  screen,
+} from "@testing-library/react";
 
 import { fakeHttpLib } from "../../api/fakeHttpLib";
 import { PostsWithSearch } from ".";
@@ -15,15 +20,17 @@ const postsMock = [
 describe("PostsWithSearch", () => {
   it("is initially loading", async () => {
     (fakeHttpLib.get as jest.Mock).mockResolvedValue(postsMock);
-    const { getByText } = render(<PostsWithSearch />);
-    await waitFor(() => expect(getByText("Loading...")).toBeInTheDocument());
+    render(<PostsWithSearch />);
+    await waitFor(() =>
+      expect(screen.getByText(/Loading/)).toBeInTheDocument()
+    );
   });
 
   it("has items after loading", async () => {
     (fakeHttpLib.get as jest.Mock).mockResolvedValue(postsMock);
-    const { getAllByText } = render(<PostsWithSearch />);
+    render(<PostsWithSearch />);
     await waitFor(() => {
-      const items = getAllByText(/Mocked Post [0-9]/);
+      const items = screen.getAllByText(/Mocked Post [0-9]/);
       expect(items.length).toEqual(3);
     });
   });
@@ -31,13 +38,12 @@ describe("PostsWithSearch", () => {
   describe("when user types in search input", () => {
     it("filters content accordingly", async () => {
       (fakeHttpLib.get as jest.Mock).mockResolvedValue(postsMock);
-      const { getByPlaceholderText, getAllByText } = render(
-        <PostsWithSearch />
-      );
-      userEvent.type(getByPlaceholderText("Search"), "3");
+      render(<PostsWithSearch />);
+      await waitForElementToBeRemoved(() => screen.getByText(/Loading*/));
+      await userEvent.type(screen.getByPlaceholderText("Search"), "3");
 
       await waitFor(() => {
-        const items = getAllByText(/Mocked Post/);
+        const items = screen.getAllByText(/Mocked Post/);
         expect(items.length).toEqual(1);
       });
     });
@@ -46,20 +52,19 @@ describe("PostsWithSearch", () => {
   describe("when user clears search input", () => {
     it("resets content to initial items", async () => {
       (fakeHttpLib.get as jest.Mock).mockResolvedValue(postsMock);
-      const { getByPlaceholderText, getAllByText } = render(
-        <PostsWithSearch />
-      );
-      userEvent.type(getByPlaceholderText("Search"), "3");
+      render(<PostsWithSearch />);
+      await waitForElementToBeRemoved(() => screen.getByText(/Loading*/));
+      await userEvent.type(screen.getByPlaceholderText("Search"), "3");
 
       await waitFor(() => {
-        const items = getAllByText(/Mocked Post/);
+        const items = screen.getAllByText(/Mocked Post/);
         expect(items.length).toEqual(1);
       });
 
-      userEvent.clear(getByPlaceholderText("Search"));
+      userEvent.clear(screen.getByPlaceholderText("Search"));
 
       await waitFor(() => {
-        const items = getAllByText(/Mocked Post/);
+        const items = screen.getAllByText(/Mocked Post/);
         expect(items.length).toEqual(3);
       });
     });
